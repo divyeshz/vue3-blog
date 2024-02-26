6942cdd9ac6b8f9573b1f1905568617b004426ff
Divyesh Mepal <divyeshm@zignuts.com>
2024-02-26 14:53:01 +0530

    Pass Data Parent to Child Component

---
+++ src/components/Child.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Child Component Name is {{ name }}</h2>
+        <h3>User : {{ user }}</h3>
+        <h3>User name : {{ user.name }}</h3>
+        <h3>User email : {{ user.email }}</h3>
+        <button @click="getData()"> Call Parent component Function</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Child',
+    props: {
+        name: String,
+        user: Object,
+        getData: Function
+    }
+}
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,51 +1,30 @@
 <template>
     <div>
-        <h1>For loop</h1>
-        <ul v-for="tech in technology" :key="tech">
-            <li> {{ tech }}</li>
-        </ul>
-
-        <br />
-        <br />
-        <ul>
-            <li v-for="user in users" :key="user.name">
-                Name is : {{ user.name }} and
-                Email is : {{ user.email }}
-            </li>
-        </ul>
+        <h1>Home component</h1>
+        <Child name="Divyesh" :user="user" :getData="getData" />
     </div>
 </template>
 
 <script>
+import Child from './Child.vue'
 
 export default {
     name: 'Home',
     data() {
         return {
-            technology: ['Java', 'PHP', 'Vue', 'Node', 'Html'],
-            users: [
-                {
-                    name: "Divyesh",
-                    email: "test@gmail.com"
-                },
-                {
-                    name: "Neel",
-                    email: "test1@gmail.com"
-                },
-                {
-                    name: "Ravi",
-                    email: "test3@gmail.com"
-                },
-                {
-                    name: "Yash",
-                    email: "test4@gmail.com"
-                },
-            ]
+            user: {
+                name: 'Divyesh', email: 'd@gmail.com'
+            }
         }
     },
     methods: {
-
+        getData(){
+            console.log('Call');
+        }
     },
+    components: {
+        Child
+    }
 }
 </script>
 
