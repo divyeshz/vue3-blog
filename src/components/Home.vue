10e38d0072235a119255218d022819805a750037
Divyesh Mepal <divyeshm@zignuts.com>
2024-02-26 12:30:51 +0530

    Event

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
-        <h1>Email {{ email }}</h1>
-        <h1>Name {{ getName('Neel Joshi') }}</h1>
-        <h1>Get Data :- {{ getData().email }}</h1>
+        <button @dblclick="test()">Double Click Me</button>
+        <button @click="test()">Click Me</button>
+
+        <button @click="countClick()">Count Click</button>
+        <h2>Button Clicked {{ count }} times</h2>
     </div>
 </template>
 
@@ -12,12 +14,16 @@ export default {
     name: 'Home',
     data() {
         return {
+            count: 0,
             email: 'test@gmail.com',
         }
     },
     methods: {
-        getName(name) {
-            return name
+        test() {
+            alert('Function Called')
+        },
+        countClick() {
+            this.count++;
         },
         getData() {
             return {
