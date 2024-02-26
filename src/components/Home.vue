7c0511b708d9e464c2e460af21f7ca0e86b484ea
Divyesh Mepal <divyeshm@zignuts.com>
2024-02-26 14:20:16 +0530

    Get Checkbox & Radio Button Value

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,30 @@
 <template>
     <div>
-        <input type="text" v-model="text" />
-        <h2>{{ text }}</h2>
+        <h1>Get Checkbox & Radio Button Value</h1>
+        <h3>Technology</h3>
+        <label for="java">Java</label>
+        <input type="checkbox" name="" id="java" v-model="technology" value="Java">
+        <br />
+        <br />
+        <label for="php">PHP</label>
+        <input type="checkbox" name="" v-model="technology" id="php" value="PHP">
+        <br />
+        <br />
+        <label for="vue">Vue</label>
+        <input type="checkbox" name="" v-model="technology" id="vue" value="Vue">
+        <br />
+        <h4>selected Technology :- {{ technology }}</h4>
+
+        <h3>Gender</h3>
+        <label for="male">Male</label>
+        <input type="radio" value="Male" name="gender" id="male" v-model="gender">
+        <br />
+        <br />
+        <label for="female">Female</label>
+        <input type="radio" value="Female" name="gender" id="female" v-model="gender">
+        <br />
+        <h4>selected Gender :- {{ gender }}</h4>
+
     </div>
 </template>
 
@@ -11,7 +34,8 @@ export default {
     name: 'Home',
     data() {
         return {
-            text: null,
+            technology: [],
+            gender: null
         }
     },
     methods: {
@@ -20,6 +44,9 @@ export default {
         },
         countClick() {
             this.count++;
+        },
+        getValues() {
+
         },
         getData() {
             return {
