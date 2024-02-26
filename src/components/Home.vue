3062bc51fb18f1649d3fc75f9619779345e7ed47
Divyesh Mepal <divyeshm@zignuts.com>
2024-02-26 14:28:02 +0530

    if else conditions

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,30 +1,9 @@
 <template>
     <div>
-        <h1>Get Checkbox & Radio Button Value</h1>
-        <h3>Technology</h3>
-        <label for="java">Java</label>
-        <input type="checkbox" name="" id="java" v-model="technology" value="Java">
-        <br />
-        <br />
-        <label for="php">PHP</label>
-        <input type="checkbox" name="" v-model="technology" id="php" value="PHP">
-        <br />
-        <br />
-        <label for="vue">Vue</label>
-        <input type="checkbox" name="" v-model="technology" id="vue" value="Vue">
-        <br />
-        <h4>selected Technology :- {{ technology }}</h4>
+        <h1 v-if="show">If Else Condition</h1>
+        <h1 v-else>Else Condition</h1>
 
-        <h3>Gender</h3>
-        <label for="male">Male</label>
-        <input type="radio" value="Male" name="gender" id="male" v-model="gender">
-        <br />
-        <br />
-        <label for="female">Female</label>
-        <input type="radio" value="Female" name="gender" id="female" v-model="gender">
-        <br />
-        <h4>selected Gender :- {{ gender }}</h4>
-
+        <button @click="hideAndShow">Show Else Condition</button>
     </div>
 </template>
 
@@ -34,13 +13,15 @@ export default {
     name: 'Home',
     data() {
         return {
-            technology: [],
-            gender: null
+            show: true
         }
     },
     methods: {
         test() {
             alert('Function Called')
+        },
+        hideAndShow() {
+            this.show = !this.show
         },
         countClick() {
             this.count++;
