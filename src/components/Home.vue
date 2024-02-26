03c5b6be00b31df1e20bdfb5b3a4e4f3bf53c9f3
Divyesh Mepal <divyeshm@zignuts.com>
2024-02-26 15:28:01 +0530

    Reuse Component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,29 +1,46 @@
 <template>
     <div>
         <h1>Home component</h1>
-        <Child name="Divyesh" :user="user" :getData="getData" />
+
+        <ul>
+            <li v-for="user in users" :key="user.name">
+                <User :user="user" :getData="getData" />
+            </li>
+        </ul>
+
     </div>
 </template>
 
 <script>
-import Child from './Child.vue'
+import User from './User.vue';
 
 export default {
     name: 'Home',
     data() {
         return {
-            user: {
-                name: 'Divyesh', email: 'd@gmail.com'
-            }
+            users: [
+                {
+                    name: 'Divyesh', email: 'd@gmail.com'
+                },
+                {
+                    name: 'Neel', email: 'n@gmail.com'
+                },
+                {
+                    name: 'Yash', email: 'y@gmail.com'
+                },
+                {
+                    name: 'Ravi', email: 'r@gmail.com'
+                },
+            ]
         }
     },
     methods: {
-        getData(){
-            console.log('Call');
+        getData(name) {
+            alert(name);
         }
     },
     components: {
-        Child
+        User
     }
 }
 </script>
---
+++ src/components/User.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h3>
+            Name Is {{ user.name }} And Email Is {{ user.email }}
+        </h3>
+        <button @click.prevent="getData(user.name)"> Get Name</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'User',
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        user: Object,
+        getData: Function,
+    }
+}
+</script>
+
+<style scoped></style>
