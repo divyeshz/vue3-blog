aa6a51cc6dd2aff3f78453e9d9eb53394cbfe7b5
Divyesh Mepal <divyeshm@zignuts.com>
2024-02-26 11:54:16 +0530

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <HelloWorld msg="You did it!" />
+        <Home />
+    </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue';
+import Home from '@/components/Home.vue';
+
+export default {
+    name: 'HomeView',
+    components: {
+        HelloWorld,
+        Home,
+    }
+}
+</script>
+
+<style scoped></style>
