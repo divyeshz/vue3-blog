ee8602f941621bf0f6bd7b87e547fc2d2d66ad98
Divyesh Mepal <divyeshm@zignuts.com>
2024-02-26 14:38:05 +0530

    for loop

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,18 @@
 <template>
     <div>
-        <h1 v-if="show">If Else Condition</h1>
-        <h1 v-else>Else Condition</h1>
+        <h1>For loop</h1>
+        <ul v-for="tech in technology" :key="tech">
+            <li> {{ tech }}</li>
+        </ul>
 
-        <button @click="hideAndShow">Show Else Condition</button>
+        <br />
+        <br />
+        <ul>
+            <li v-for="user in users" :key="user.name">
+                Name is : {{ user.name }} and
+                Email is : {{ user.email }}
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -13,28 +22,29 @@ export default {
     name: 'Home',
     data() {
         return {
-            show: true
+            technology: ['Java', 'PHP', 'Vue', 'Node', 'Html'],
+            users: [
+                {
+                    name: "Divyesh",
+                    email: "test@gmail.com"
+                },
+                {
+                    name: "Neel",
+                    email: "test1@gmail.com"
+                },
+                {
+                    name: "Ravi",
+                    email: "test3@gmail.com"
+                },
+                {
+                    name: "Yash",
+                    email: "test4@gmail.com"
+                },
+            ]
         }
     },
     methods: {
-        test() {
-            alert('Function Called')
-        },
-        hideAndShow() {
-            this.show = !this.show
-        },
-        countClick() {
-            this.count++;
-        },
-        getValues() {
 
-        },
-        getData() {
-            return {
-                name: 'Divyesh',
-                email: this.email
-            }
-        }
     },
 }
 </script>
